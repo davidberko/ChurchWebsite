c10ddf6927edb352b6beebcac54f583c73a42b37
David Michael Berko <davidberko19@gmail.com>
2012-10-17 16:08:05 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChurchWebsite::Application.config.secret_token = '8d8910d8508c7e48423ed6d69da0fe0aef804bbb4c62a4544fcaa4b3dfb6742ebbec9b424c47b50eabc5ce9f7e0c0515993b8ee2dd71bd85019afa3c4bba5525'
